58df499f3518c25051fbedca951d1d417ae001fa
AnveshSrivastava <anveshsrivastava9@gmail.com>
2025-04-28 16:22:17 +0530

    map and destructuring

---
+++ map.js
@@ -0,0 +1,116 @@
+
+// const fruits= new Map([
+//     ['apple', 1],
+//     ['banana', 2],
+//     ['orange', 3]
+// ]);
+// console.log(fruits);
+
+
+// const fruits= new Map();
+// fruits.set('apple', 1);
+// fruits.set('banana', 2);
+// fruits.set('orange', 3);
+// console.log(fruits);
+
+// fruits.get('banana'); 
+// console.log(typeof fruits.get('banana'));
+
+// fruits.delete('banana');
+// console.log(fruits);
+// fruits.clear();
+// console.log(fruits);
+// fruits.set('apple', 100);
+// fruits.set('banana', 200); 
+// fruits.set('orange', 300);
+// console.log(fruits);
+
+
+
+// let text="";
+// fruits.forEach(function(value, key){
+//     text += key + "=" + value + "\n";
+// })
+// console.log(text);
+
+// for(const x of fruits.entries()){
+//     text += x + "\n";
+// }
+// console.log(text);
+
+// for(const x of fruits.keys()){
+//     text += x + "\n";
+// }
+// console.log(text);
+// let sum=0;
+// for(const x of fruits){
+//     sum += x[1];
+// }
+// console.log(sum);
+// for(const x of fruits.values()){
+//     text += x + "\n";
+// }
+// console.log(text);
+
+// const apple={name:"apple"}
+// const banana={name:"banana"}
+// const orange={name:"orange"}
+
+// const fruits= new Map();
+
+// fruits.set(apple, 1);
+// fruits.set(banana, 2);
+// fruits.set(orange, 3);
+// console.log(fruits);
+// fruits.get(apple);
+// console.log(fruits.get("apple"));
+
+// const fruits = [
+//     { name: 'apple', quantity: 1 },
+//     { name: 'banana', quantity: 2 },
+//     { name: 'orange', quantity: 3 } 
+// ];
+
+// function myCallback({quantity}) {
+//     return quantity > 2? "ok": "low";
+// }
+// const result Map.groupby(fruits, myCallback);
+// console.log(result);
+
+// const person={
+//     firstName:"Morris",
+//     lastName:"Kathuloo",
+//     age: 25,
+// }
+
+// let {lastName,firstName}= person
+// console.log(firstName);
+// console.log(lastName);
+
+// let name ="Morris";
+// let [a1,a2]=name
+// console.log(a1);
+// let {[2]:a3}=name;
+// console.log(a3);
+
+// const numbers=[1,2,3,4,5,6,7,8,9,10];
+// let v=numbers.includes(5);
+// console.log(v);
+// let w= numbers.includes(5,2);
+// console.log(w);
+// const [a,b,...rest]=numbers;
+// console.log(rest);
+
+//swapping
+// let firstName="Morris";
+// let lastName="Kathuloo";
+// [firstName,lastName]=[lastName,firstName];
+// console.log(firstName);
+// console.log(lastName);
+
+// let x=5;
+// console.log(x**2);
+
+//trailing comma
+
+
